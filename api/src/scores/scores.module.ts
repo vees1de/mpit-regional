f265ec613cf345813313993243e7b9e8d23aaaf6
Едисеев Максим Иванович <ediseev_mi@albank.ru>
2025-12-06 10:00:16 +0900

    fix

--- api/src/scores/scores.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { ScoresController } from "./scores.controller";
-import { ScoresService } from "./scores.service";
-
-@Module({
-  controllers: [ScoresController],
-  providers: [ScoresService],
-})
-export class ScoresModule {}
